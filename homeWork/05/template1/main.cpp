664b2210788f7a72269cd3b96691bebe55fbc455
RomanRakov <144253949+RomanRakov@users.noreply.github.com>
2025-05-16 13:18:20 +0000

    hw by Rakov

--- homeWork/05/template1/main.cpp
+++ homeWork/05/template1/main.cpp
@@ -4,5 +4,6 @@
 using namespace std;
 
 int main(){
+    std::cout << "fibonacci<6>() = " << fibonacci<6>() << std::endl;
     return 0;
 }
--- homeWork/05/template1/template.hpp
+++ homeWork/05/template1/template.hpp
@@ -1,13 +1,37 @@
-#pragma once 
+#pragma once
 #include <cmath>
+
 #define EPS 1e-10
 
-
-auto fibonacci() -> T{
-
+template <int N>
+constexpr int fibonacci() {
+    return fibonacci<N - 1>() + fibonacci<N - 2>();
 }
 
+template <>
+constexpr int fibonacci<0>() {
+    return 0;
+}
 
-class Fibonacci{
+template <>
+constexpr int fibonacci<1>() {
+    return 1;
+}
 
+template <int N>
+class Fibonacci {
+public:
+    static const int value = Fibonacci<N - 1>::value + Fibonacci<N - 2>::value;
+};
+
+template <>
+class Fibonacci<0> {
+public:
+    static const int value = 0;
+};
+
+template <>
+class Fibonacci<1> {
+public:
+    static const int value = 1;
 };
